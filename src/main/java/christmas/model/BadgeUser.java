422fba742d732e92a0b00d9de154c7e665017888
shining013 <nevertheless113@naver.com>
2023-11-15 16:38:34 +0900

    feat: 뱃지 사용 클래스 생성

---
+++ src/main/java/christmas/model/BadgeUser.java
@@ -0,0 +1,20 @@
+package christmas.model;
+
+public class BadgeUser {
+    public String calculateBadge(double total) {
+        Badge badge = Badge.NON;
+        if (total >= 5000) {
+            badge = Badge.STAR;
+        }
+        if (total >= 10000) {
+            badge = Badge.TREE;
+        }
+        if (total >= 20000) {
+            badge = Badge.SANTA;
+        }
+        if (total <= 5000) {
+            badge = Badge.NON;
+        }
+        return badge.getBadgeName(badge);
+    }
+}
