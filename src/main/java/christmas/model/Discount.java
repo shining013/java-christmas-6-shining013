0aaf4946ad64349ef39be8767fa3dc1b815bfd79
shining013 <nevertheless113@naver.com>
2023-11-15 17:39:33 +0900

    feat : 할인 주관하는 Discount 클래스 생성
    1. eventApply 메서드 : 할인 적용 전 금액 1만원 미만인 경우 메세지 출력하고 종료. 1만원 이상인 경우 applyDiscount 메서드 실행
    
    2. applyDiscount 메서드
    크리스마스, 특별, 증정, 평일, 주말 할인 계산
    
    3. calculateWeekDayDiscount() : 평일 할인가 계산
    4. calculateWeekEndDiscount() : 주말 할인가 계산
    5. getChristmasDayDiscountAmount : 크리스마스 할인가 리턴
    6. getWeekDayDiscountAmount : 평일 할인가 리턴
    7. getWeekEndDiscountAmount : 주말 할인가 리턴
    8. getSpecialDayAmount : 특별 할인가 리턴
    9. getGiftAmount : 증정 이벤트 적용가격 리턴

---
+++ src/main/java/christmas/model/Discount.java
@@ -0,0 +1,78 @@
+package christmas.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import christmas.Constants;
+
+public class Discount {
+    private final double beforeDiscountAmount;
+    private final int visitDate;
+    private double christmasDayDiscountAmount = 0;
+    private double weekDayDiscountAmount = 0;
+    private double weekEndDiscountAmount = 0;
+    private double specialDayAmount = 0;
+    private double giftAmount = 0;
+    private Map<String, Integer> getOrder = new HashMap<>();
+    Calendar calendar = new Calendar();
+    MenuBoard menuBoard = new MenuBoard();
+
+    public Discount(double beforeDiscountAmount, int visitDate, Map<String, Integer> order) {
+        this.beforeDiscountAmount = beforeDiscountAmount;
+        this.visitDate = visitDate;
+        this.getOrder = order;
+        eventApply();
+    }
+
+    public void eventApply() {
+        if (beforeDiscountAmount < 10000) {
+            System.out.println(Constants.EVENT_DO_NOT_APPLY);
+            return;
+        }
+        applyDiscount();
+    }
+
+    public void applyDiscount() {
+        christmasDayDiscountAmount = calendar.christmasDiscountApply(visitDate);
+        specialDayAmount = calendar.specialDiscountApply(visitDate);
+        this.giftAmount = calendar.giftApply(beforeDiscountAmount);
+        if (calendar.weekDayApply(visitDate)) {
+            calculateWeekDayDiscount();
+        }
+        if (!calendar.weekDayApply(visitDate)) {
+            calculateWeekEndDiscount();
+        }
+    }
+
+    public void calculateWeekDayDiscount() {
+        for (String key : getOrder.keySet()) {
+            weekDayDiscountAmount += menuBoard.calculateDessertAmount(key, getOrder.get(key));
+        }
+    }
+
+    public void calculateWeekEndDiscount() {
+        for (String key : getOrder.keySet()) {
+            weekEndDiscountAmount += menuBoard.calculateMainDish(key, getOrder.get(key));
+        }
+    }
+
+    public double getChristmasDayDiscountAmount() {
+        return christmasDayDiscountAmount;
+    }
+
+    public double getWeekDayDiscountAmount() {
+        return weekDayDiscountAmount;
+    }
+
+    public double getWeekEndDiscountAmount() {
+        return weekEndDiscountAmount;
+    }
+
+    public double getSpecialDayAmount() {
+        return specialDayAmount;
+    }
+
+    public double getGiftAmount() {
+        return giftAmount;
+    }
+}
