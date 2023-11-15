4c86d13ceb5f1d7000f04f3f9c0eef3a5e6e89ff
shining013 <nevertheless113@naver.com>
2023-11-15 16:33:38 +0900

    feat: 주문 입력/유효성 검사 메서드 생성

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,71 @@
+package christmas.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import christmas.Constants;
+
+public class Order {
+    public static Map<String, Integer> ITEMS;
+    private final MenuBoard menuBoard = new MenuBoard();
+    private double beforeDiscountAmount=0;
+
+    public Order(String enterOrder) {
+        ITEMS = new HashMap<>();
+        setOrder(enterOrder);
+        setBeforeDiscountAmount();
+    }
+
+    public List<String> getOrderMenu() {
+        return new ArrayList<>(ITEMS.keySet());
+    }
+
+    public Map<String, Integer> getMenu() {
+        return ITEMS;
+    }
+
+    public void addItem(String menu, int quantity) {
+        if (ITEMS.containsKey(menu)) {
+            throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+        }
+        ITEMS.put(menu,quantity);
+    }
+
+    public void setOrder(String enterOrder) {
+        String[] orders = enterOrder.split(Constants.DELIMITER);
+        for (String orderStr : orders) {
+            String[] orderParts = orderStr.split(Constants.DASH);
+            if (orderParts.length == 2) {
+                setOrderBoard(orderParts);
+            }
+        }
+    }
+
+    public void setOrderBoard(String[] order) {
+        String menu = order[0];
+        int quantity = Integer.parseInt(order[1]);
+        addItem(menu,quantity);
+    }
+
+    public void setBeforeDiscountAmount() {
+        for (String str : getOrderMenu()) {
+            double price = menuBoard.getMenuPrice(str);
+            int quantity = getQuantity(str);
+            beforeDiscountAmount += price * quantity;
+        }
+    }
+
+    public double getBeforeDiscountAmount() {
+        return beforeDiscountAmount;
+    }
+
+    public double getTotalBenefitAmount(double christmas, double weekDay, double weekEnd, double special, double gift) {
+        return christmas+weekDay+weekEnd+special+gift;
+    }
+
+    public int getQuantity(String menu) {
+        return ITEMS.get(menu);
+    }
+}
---
+++ src/main/java/christmas/model/OrderValidator.java
@@ -0,0 +1,101 @@
+package christmas.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+
+import christmas.Constants;
+
+public class OrderValidator {
+    private final Map<String, Integer> ITEMS = new HashMap<>();
+    private final String ENTER_ORDER;
+    private final MenuBoard menuBoard = new MenuBoard();
+
+    public OrderValidator(String enterOrder) {
+        this.ENTER_ORDER = enterOrder;
+        setToOrder(this.ENTER_ORDER);
+        validate();
+    }
+
+    public String getString() {
+        return ENTER_ORDER;
+    }
+
+    public void addItem(String menu, int quantity) {
+        if (ITEMS.containsKey(menu)) {
+            throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+        }
+        ITEMS.put(menu, quantity);
+    }
+
+    public void setToOrder(String enterOrder) {
+        String[] orders = enterOrder.split(Constants.DELIMITER);
+        for (String orderStr : orders) {
+            String[] orderParts = orderStr.split(Constants.DASH);
+            if (orderParts.length == 2) {
+                String menu = orderParts[0];
+                int quantity = Integer.parseInt(orderParts[1]);
+                addItem(menu, quantity);
+            }
+        }
+    }
+
+    public void validate() {
+        isValidOrderType();
+        isMenuExist();
+        isQuantityMoreThanOne();
+        isTotalQuantityExceed20();
+        isOrderIsAllBeverage();
+    }
+
+    public void isValidOrderType() {
+        Matcher matcher = Constants.ORDER_PATTERN.matcher(ENTER_ORDER);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+        }
+    }
+
+    public void isMenuExist() {
+        for (Map.Entry<String, Integer> entry : ITEMS.entrySet()) {
+            String menuName = entry.getKey();
+            MenuItem foundMenu = menuBoard.findMenuByName(menuName);
+            if (foundMenu == null) {
+                throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+            }
+        }
+    }
+
+    public void isQuantityMoreThanOne() {
+        for (Map.Entry<String, Integer> entry : ITEMS.entrySet()) {
+            int quantity = entry.getValue();
+            if (quantity < 1) {
+                throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+            }
+        }
+    }
+
+    public void isTotalQuantityExceed20() {
+        int totalQuantity = 0;
+        for (Map.Entry<String, Integer> entry : ITEMS.entrySet()) {
+            int quantity = entry.getValue();
+            if (totalQuantity + quantity > 20) {
+                throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+            }
+            totalQuantity += quantity;
+        }
+    }
+
+    public void isOrderIsAllBeverage() {
+        boolean allBeverages = true;
+        for (String menu : ITEMS.keySet()) {
+            MenuItem menuItem = menuBoard.findMenuByName(menu);
+            if (!menuItem.getCategory().equalsIgnoreCase("beverage")) {
+                allBeverages = false;
+                break;
+            }
+        }
+        if (allBeverages) {
+            throw new IllegalArgumentException(Constants.INVALID_ORDER_ERROR);
+        }
+    }
+}
