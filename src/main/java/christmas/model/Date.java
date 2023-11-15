6808436d39e8c4f729c5f16365087490a950cfa1
shining013 <nevertheless113@naver.com>
2023-11-15 19:04:15 +0900

    modify: Date 클래스에 toString 메서드 오버라이딩

--- src/main/java/christmas/model/Date.java
+++ src/main/java/christmas/model/Date.java
@@ -28,4 +28,9 @@ public class Date {
     public boolean isWeekDay() {
         return isWeekDay;
     }
+
+    @Override
+    public String toString() {
+        return String.format("날짜 (날짜 = %d, 크리스마스 디데이? = %b, 특별 할인? = %b, 평일? = %b)",date,isChristmasD_dayDiscount,isSpecialDiscountDate,isWeekDay);
+    }
 }
